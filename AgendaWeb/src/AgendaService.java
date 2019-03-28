4132f4b7293dca795720838cd7a1dd47d98c9b2f
LucSandes <45044738+LucSandes@users.noreply.github.com>
2019-03-28 14:25:13 -0300

    Add files via upload

---
+++ AgendaWeb/src/AgendaService.java
@@ -0,0 +1,25 @@
+
+public class AgendaService extends Agenda {
+	AgendaDAO dao;
+
+	public void Agenda() {
+		dao = new AgendaDAO();
+	}
+
+	public void criar(Agenda to) {
+		dao.incluir(to);
+	}
+
+	public void atualizar(Agenda to, String novoTelefone) {
+		dao.atualizar(to, novoTelefone);
+	}
+
+	public void excluir(Agenda to) {
+		dao.excluir(to);
+	}
+
+	public Agenda carregar(Agenda to) {
+		return dao.carregar(to);
+	}
+
+}
